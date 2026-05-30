3132377c8adb0637011e9fa2fd000405a72a73ab
sanyakaushik <70109135+sanyakaushik@users.noreply.github.com>
2026-05-30 10:13:55 -0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/sanya-portfolio",
+  assetPrefix: "/sanya-portfolio/",
+};
+
+module.exports = nextConfig;
